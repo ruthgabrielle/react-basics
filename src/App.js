325777d0c812174d29b1af69c97af6275cdea13e
ruthgabrielle <ruthfgms@gmail.com>
2021-05-14 11:08:22 -0300

    Adding Form Inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import SelfExpenses from './components/Expenses/SelfExpenses';
+import NewExpense from './components/NewExpense/NewExpense'
 
 const App = () => {
   const expenses = [
@@ -30,7 +31,7 @@ const App = () => {
   return (
     <div>
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <SelfExpenses items={expenses}/>
     </div>
     </div>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,26 @@
+import React, { useState } from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
-function ExpenseItem(props) {
+
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+    console.log('ExpenseItem evaluated by React')
+
+
+    const clickHandler = () => {
+        setTitle('Uptaded')
+        console.log(title)
+    };
 
     return (
         <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2> {props.title} </h2>
+                <h2> {title} </h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
