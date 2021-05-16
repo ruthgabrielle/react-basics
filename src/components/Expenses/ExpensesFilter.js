8b8447ab4c206438b710071a6c4ddbf9cb3fa9d7
ruthgabrielle <ruthfgms@gmail.com>
2021-05-16 11:08:46 -0300

    Finished assignment 2

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Card from '../UI/Card';
 
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
 
-    const filterChange = (event) => {
-        console.log(event.target.value)
+    const filterDropdownChange = (event) => {
+        props.onChangeFilter(event.target.value)
     }
 
     return (
@@ -14,7 +13,7 @@ const ExpensesFilter = () => {
             <div className='expenses-filter'>
                 <div className='expenses-filter__control'>
                     <label>Filter by year</label>
-                    <select onChange={filterChange}>
+                    <select value={props.selected} onChange={filterDropdownChange}>
                         <option value='2023'>2023</option>
                         <option value='2022'>2022</option>
                         <option value='2021'>2021</option>
